6295c21791bc4e3e7b79ec66a8bda9c04156ef1e
vladncode <vladncode@gmail.com>
2022-03-08 19:10:45 +0200

    serving statics with server now

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -11,8 +12,12 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes
 app.use(planetsRouter);
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
 
 module.exports = app;
